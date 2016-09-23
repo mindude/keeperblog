be76092895d7d35e248d256e312faa7bde6a3551
mindude <eleal@cetys.edu.mx>
2016-09-23 08:36:22 -0700

    Set up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keeperblogApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the keeperblogApp
+ */
+angular.module('keeperblogApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keeperblogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the keeperblogApp
+ */
+angular.module('keeperblogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
